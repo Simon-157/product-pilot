b285ade5784fea4792ef51404917a4011eba522b
Simon-157 <boatengsimonjnr157@gmail.com>
2023-04-27 04:39:58 +0000

    CI/CD set up

---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1DoKsd2rBX1jcosAuNbvl-ee_m_aYAAI",
+  authDomain: "product-pilot.firebaseapp.com",
+  projectId: "product-pilot",
+  storageBucket: "product-pilot.appspot.com",
+  messagingSenderId: "140875741642",
+  appId: "1:140875741642:web:60b7706b9cdb3afa573733"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
